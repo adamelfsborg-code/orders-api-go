b0949543ec2a760a550420bbf263eeab03295fd7
Adam Elfsborg <adam.elfsborg@mtek.se>
2023-09-19 15:56:04 +0200

    Feat: Implement a basic server.

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
+}
 
 func main() {
-	fmt.Println("Micro")
+	fmt.Println("Micro Service API powered by Go")
+
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: http.HandlerFunc(basicHandler),
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal("Failed to serve Server due to: ", err, "\n")
+	}
 }
