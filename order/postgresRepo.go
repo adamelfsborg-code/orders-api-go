c9d51109c61fb3b31ce50feecbd92a973a1b2518
Adam Elfsborg <adam.elfsborg@mtek.se>
2023-09-21 19:38:00 +0200

    Feat: Handle List orders.

--- order/postgresRepo.go
+++ order/postgresRepo.go
@@ -28,7 +28,19 @@ func (p *PostgresRepo) Create(ctx context.Context, order Order) error {
 }
 
 func (p *PostgresRepo) List(ctx context.Context, page FindAllPage) (FindResult, error) {
-	return FindResult{}, nil
+	var orders []Order
+
+	query := p.Client.Model(&orders).OrderExpr("order_id asc").Limit(int(page.Size)).Offset(int(page.Offset))
+
+	err := query.Select()
+	if err != nil {
+		return FindResult{}, fmt.Errorf("Failed to get orders: %w", err)
+	}
+
+	return FindResult{
+		Orders: orders,
+		Cursor: page.Offset + page.Size,
+	}, nil
 }
 
 func (p *PostgresRepo) FindByID(ctx context.Context, id uint64) (Order, error) {
