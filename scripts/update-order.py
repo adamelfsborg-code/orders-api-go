028ea5f02bee864e54ca5c24e21f747ba91c960b
Adam Elfsborg <adam.elfsborg@mtek.se>
2023-09-23 11:43:18 +0200

    feat: Handle Errors in update-order script.

--- scripts/update-order.py
+++ scripts/update-order.py
@@ -1,16 +1,21 @@
 import requests
 import sys
+from utils.log import Log, LogEx
 
 try:
     order_id = sys.argv[1]
     order_status = sys.argv[2]
+    if order_status not in ["shipped", "completed"]:
+        LogEx(f"[ValueError]: Wrong Status passed", "[Usage]: python script.py <integer_value: Order Id> <string: Status>", code=1)
 except ValueError as err:
-    print("Usage: python script.py <integer_value> <string>")
-    sys.exit(1)
+    LogEx(f"[ValueError]: {err}", "[Usage]: python script.py <integer_value: Order Id> <string: Status>", code=1)
+except IndexError as err:
+    LogEx(f"[IndexError]: {err}", "[Usage]: python script.py <integer_value: Order Id> <string: Status>", code=1)
+
 
 order = {
     "status": order_status,
 }
 
 r = requests.put(f"http://localhost:3000/orders/{order_id}", json=order)
-print("Response", r.status_code)
+LogEx(f"[Response Code]: {r.status_code}", code=r.status_code)
